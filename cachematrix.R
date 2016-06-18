31ab32955b7c534204410ddcc894bb2f7a23e231
Dmytro Artsishevskyi <artsishevsky@gmail.com>
2016-06-18 21:33:39 +0200

    Coursera R programming language w3 assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Here we're trying to write a wrapper object for square matrix, that will
+## contain computed inversed matrix. Inversed matrix will be computed on-demand,
+## computation should happen only once.
 
-## Write a short comment describing this function
+## Wrap matrix given with object, which may contain reversed matrix alongside
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	s <- NULL
+	set <- function(y) {
+		x <<- y
+		s <<- NULL
+	}
+	get <- function() x
+	setSolved <- function(solved) s <<- solved
+	getSolved <- function() s
+	list(set = set, get = get, setSolved = setSolved, getSolved = getSolved)
 }
 
 
-## Write a short comment describing this function
+## Solves given wrapped matrix, i.e. calculates it's reversed matrix (if it wasn't already been calculated)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	s <- x$getSolved()
+	if(!is.null(s)) {
+		message("Getting cached data")
+		return(s)
+	}
+	data <- x$get()
+	s <- solve(data)
+	x$setSolved(s)
+	s
 }
